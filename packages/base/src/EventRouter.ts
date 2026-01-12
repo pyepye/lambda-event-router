571a01fd98bc890155cd80e7eeac696e5d0cc83e
Matt Pye <pyematt@gmail.com>
2026-01-12 21:48:12 +0000

    Add base EventRouter from  lambda-event-manager core

---
+++ packages/base/src/EventRouter.ts
@@ -0,0 +1,31 @@
+import type { Context, Handler } from "aws-lambda";
+
+export interface EventTypeRouter<TEvent = unknown, TResult = unknown> {
+  canHandle(event: unknown): event is TEvent;
+  handleEvent(event: TEvent, context: Context): Promise<TResult>;
+}
+
+export class EventRouter {
+  private routers: EventTypeRouter[];
+
+  constructor(options: { routers: EventTypeRouter[] }) {
+    this.routers = options.routers;
+  }
+
+  handler(): Handler {
+    return async (event, context): Promise<unknown> => {
+      for (const router of this.routers) {
+        if (router.canHandle(event)) {
+          return router.handleEvent(event, context);
+        }
+      }
+      throw new Error("No router found for event");
+    };
+  }
+}
+
+export function createEventRouter(options: {
+  routers: EventTypeRouter[];
+}): EventRouter {
+  return new EventRouter(options);
+}
