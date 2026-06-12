b885251377f0dddf4eec41272a185a5993a75333
Matt Pye <pyematt@gmail.com>
2026-06-12 02:14:02 +0100

    Run pnpm lint to fix organizeImports

--- packages/appsync/vitest.config.ts
+++ packages/appsync/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
+
 import baseConfig from '../../vitest.config.base.js';
 
 export default mergeConfig(baseConfig, defineConfig({}));
--- packages/eventbridge/tsup.config.ts
+++ packages/eventbridge/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+
 import { baseConfig } from '../../tsup.config.base.js';
 
 export default defineConfig({
